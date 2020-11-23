fb7815b28c00efe953fc52ad0c856921a30a5c02
Destoky <adestoky@gmail.com>
2020-11-23 23:01:00 +0100

    Son et Score
    ajout d'un affichage de score qui marche et d'un timer ainsi qu'un son jouer au click sur une taupe

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
+//SELECTION DU DOM
 let taupes = document.querySelectorAll(".taupe");
+let score = document.querySelector(".score");
+let coup = document.getElementById("SonCoup");
+let nbrPoints = 0;
 
-
-let apparition = setInterval(()=> {
-  let nbrRandom = Math.floor(Math.random()*taupes.length);
+//Apparition des taupes de manières aléatoires
+let apparition = setInterval(() => {
+  let nbrRandom = Math.floor(Math.random() * taupes.length);
   taupes[nbrRandom].style.top = "20px";
-  setTimeout(()=> {
+  setTimeout(() => {
     taupes[nbrRandom].style.top = "100px";
-  },1000);
-},1000)
+  }, 6000);
+}, 1000);
 
 for (const taupe of taupes) {
-  taupe.addEventListener("click",()=> {
-    taupe.style.cursor =  "url('test4.png'),auto";
-    setTimeout(()=> {
-      taupe.style.cursor =  "url('test3.png'),auto";
-    },200);
-  })
+  //Quand on clique sur une taupe ..
+
+  taupe.addEventListener("click", () => {
+    //on change le cursuer pour 200ms
+    taupe.style.cursor = "url('test4.png'),auto";
+    setTimeout(() => {
+      taupe.style.cursor = "url('test3.png'),auto";
+    }, 200);
+
+    //on monte le score et on fait redescendre la taupe pour éviter de cliquer
+    //plusieurs fois et de gagner 10 points sur la même taupe par exemple
+    nbrPoints++;
+    score.innerHTML = `Score : ${nbrPoints}`;
+    taupe.style.top = "100px";
+    coup.play();
+  });
 }
